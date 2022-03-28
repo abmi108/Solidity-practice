57ddf608631530a1a1364c35b9f698670783c16f
“Abhijit <hello.abhijitmishra@gmail.com>
2022-03-28 14:57:32 +0530

    Deploying and interacting with contrct using web3.py on local blockchain

---
+++ solidity-course-patrick-collins/web3_py_simple_storage/SimpleStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+
+    uint256 favNum;
+
+    struct People {
+        uint256 favNum;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavNum;
+
+    function store(uint256 _favNum) public {
+        favNum = _favNum;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favNum;
+    }
+
+    function addPerson( string memory _name, uint256 _favNum) public {
+        people.push(People(_favNum, _name));
+        nameToFavNum[_name] = _favNum;
+    }
+
+
+}
