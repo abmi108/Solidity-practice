73c90e6d5011f11210c5c2ae0302d2f1c89fa314
“Abhijit <hello.abhijitmishra@gmail.com>
2022-03-31 15:14:18 +0530

    brownie fund me code

---
+++ solidity-course-patrick-collins/brownie_fund_me/scripts/deploy.py
@@ -0,0 +1,32 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the pricefeed address to our fund me contract
+
+    # if we are o a persistent network like rinkeby, use the associated address
+    # otherwise use mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
